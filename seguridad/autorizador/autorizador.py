cc74df458fe2e837b9b41debade8c5c32f38a9d5
Angel Henao <ay.henao@uniandes.edu.co>
2025-03-13 23:49:46 -0500

    Se configura oauth2 en el message broker con Autorizador y se registra el detector de instrusos de manera correcta

--- seguridad/autorizador/autorizador.py
+++ seguridad/autorizador/autorizador.py
@@ -1,17 +1,44 @@
+import base64
+import hashlib
+import json
 import logging
 import os
+from datetime import datetime, timedelta
 from typing import Any
 
+import jwt
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey
 from fastapi import FastAPI, HTTPException
-from jwt import InvalidSignatureError, decode
+from jwt import InvalidSignatureError
 from pydantic import BaseModel
+from starlette.responses import JSONResponse
 
-PERMISOS = {
+PUERTO = int(os.getenv("AUTORIZADOR_PUERTO", "8090"))
+AUDIENCE = os.getenv("AUDIENCE", "inventario-test")
+ALGORITHM = "RS256"
+
+PERMISOS_INVENTARIO = {
     "inventario_write": {"POST": ["/productos"], "DELETE": ["/productos/<nombre>"]},
     "inventario_read": {"GET": ["/productos"]},
 }
-PRE_SHARED_KEY = os.getenv("PRE_SHARED_KEY", "")
-PUERTO = int(os.getenv("AUTORIZADOR_PUERTO", "8090"))
+PERMISOS_MESSAGE_BROKER = [
+    f"{AUDIENCE}.read:*/*/*",
+    f"{AUDIENCE}.write:*/*/*",
+    f"{AUDIENCE}.configure:*/*/*",
+]
+USUARIOS_PERMISOS = {
+    "detector": PERMISOS_MESSAGE_BROKER,
+    "publisher": PERMISOS_MESSAGE_BROKER,
+    "auditor": PERMISOS_MESSAGE_BROKER,
+    "usuario_legitimo": ["inventario_write", "inventario_read"],
+    "usuario_intruso": ["inventario_write"],
+}
+
+PRIVATE_KEY_PATH = "config/keys/private.pem"
+PUBLIC_KEY_PATH = "config/keys/public.pem"
+
 
 logging.basicConfig(
     level=logging.DEBUG,
@@ -34,10 +61,76 @@ class RespuestaAutorizacion(BaseModel):
     token: str
 
 
+class JWK(BaseModel):
+    n: str
+    e: str
+    kid: str
+    kty: str = "RSA"
+    alg: str = ALGORITHM
+
+
+class JWKS(BaseModel):
+    keys: list[JWK]
+
+
+def leer_llave_privada() -> str:
+    with open(PRIVATE_KEY_PATH, "r") as private_key_file:
+        private_key = private_key_file.read()
+    return private_key
+
+
+def leer_llave_publica() -> RSAPublicKey:
+    with open(PUBLIC_KEY_PATH, "rb") as public_key_file:
+        public_key: RSAPublicKey = serialization.load_pem_public_key(
+            public_key_file.read(), backend=default_backend()
+        )
+    return public_key
+
+
+def generar_jwks() -> JWKS:
+    public_key = leer_llave_publica()
+
+    public_numbers = public_key.public_numbers()
+    n = public_numbers.n
+    e = public_numbers.e
+    n_bytes = n.to_bytes((n.bit_length() + 7) // 8, "big")
+    e_bytes = e.to_bytes((e.bit_length() + 7) // 8, "big")
+    n_base64 = base64.urlsafe_b64encode(n_bytes).rstrip(b"=").decode("utf-8")
+    e_base64 = base64.urlsafe_b64encode(e_bytes).rstrip(b"=").decode("utf-8")
+    kid_input = f"{n_base64}{e_base64}".encode("utf-8")
+    kid = (
+        base64.urlsafe_b64encode(hashlib.sha256(kid_input).digest())
+        .rstrip(b"=")
+        .decode("utf-8")
+    )
+    return JWKS(keys=[JWK(n=n_base64, e=e_base64, kid=kid)])
+
+
+def generar_token(usuario) -> str:
+    private_key = leer_llave_privada()
+    permisos = USUARIOS_PERMISOS.get(usuario, [])
+
+    jwks = generar_jwks()
+    kid = jwks.keys[0].kid
+
+    headers = {"kid": kid, "alg": ALGORITHM, "typ": "JWT"}
+
+    payload = {
+        "sub": usuario,
+        "aud": AUDIENCE,
+        "iat": datetime.now(),
+        "exp": datetime.now() + timedelta(minutes=7200),
+        "scope": permisos,
+    }
+    token = jwt.encode(payload, private_key, algorithm=ALGORITHM, headers=headers)
+    return token
+
+
 def decodificar_token(solicitud: SolicitudAutorizacion) -> Any:
+    public_key = leer_llave_publica()
     try:
-        decoded_token = decode(
-            solicitud.token, key=PRE_SHARED_KEY, algorithms=["HS256"]
+        decoded_token = jwt.decode(
+            solicitud.token, key=public_key, algorithms=[ALGORITHM], audience=AUDIENCE
         )
     except InvalidSignatureError:
         raise
@@ -45,18 +138,24 @@ def decodificar_token(solicitud: SolicitudAutorizacion) -> Any:
 
 
 def autorizar_usuario(solicitud: SolicitudAutorizacion) -> bool:
+    autorizado = False
     decoded_token = decodificar_token(solicitud)
-    usuario = decoded_token.get("usuario")
-    permiso = decoded_token.get("permiso")
-    autorizado = all(
-        [
-            permiso in PERMISOS,
-            solicitud.method in PERMISOS.get(permiso, {}),
-            solicitud.path in PERMISOS.get(permiso, {}).get(solicitud.method, []),
-        ]
-    )
+    usuario = decoded_token.get("sub")
+    permisos = decoded_token.get("scope")
+
+    for permiso in permisos:
+        autorizado = all(
+            [
+                permiso in PERMISOS_INVENTARIO,
+                solicitud.method in PERMISOS_INVENTARIO.get(permiso, {}),
+                solicitud.path
+                in PERMISOS_INVENTARIO.get(permiso, {}).get(solicitud.method, []),
+            ]
+        )
+        if autorizado:
+            break
     logging.debug(
-        f"[Autorizador]: {usuario=} {permiso=} {solicitud.method=} {solicitud.path=} -> {autorizado=}"
+        f"[Autorizador]: {usuario=} {permisos=} {solicitud.method=} {solicitud.path=} -> {autorizado=}"
     )
     return autorizado
 
@@ -70,6 +169,34 @@ def autorizar(solicitud: SolicitudAutorizacion) -> RespuestaAutorizacion:
         return RespuestaAutorizacion(token=solicitud.token)
     except InvalidSignatureError:
         raise HTTPException(status_code=401, detail="Invalid token")
+
+
+@app.post("/token/{usuario}", response_model=RespuestaAutorizacion)
+def token_usuario(usuario: str) -> RespuestaAutorizacion:
+    token = generar_token(usuario)
+    return RespuestaAutorizacion(token=token)
+
+
+@app.get("/.well-known/jwks.json", response_model=JWKS)
+def jwks() -> JWKS:
+    return generar_jwks()
+
+
+@app.get("/health")
+async def health():
+    return "ok"
+
+
+# @app.post("/probar/jwks")
+# def probar_jwks(token: str) -> JSONResponse:
+#     jwks = generar_jwks()
+#
+#     key = jwks.keys[0]
+#     public_key = jwt.algorithms.RSAAlgorithm.from_jwk(json.dumps(key.__dict__))
+#     decoded_payload = jwt.decode(
+#         token, public_key, algorithms=["RS256"], audience=AUDIENCE
+#     )
+#     return JSONResponse(content=decoded_payload)
 
 
 if __name__ == "__main__":
--- seguridad/deteccion_intrusos/detector.py
+++ seguridad/deteccion_intrusos/detector.py
@@ -3,59 +3,63 @@ import jwt
 import json
 import logging
 import os
+
+import requests
+
 from disponibilidad.servicios.rabbitMQ.subscriptor import Subscriptor
 
 logger = logging.getLogger(__name__)
+
 
 class Detector:
 
     @property
     def lista_blanca(self):
         return "This is a readonly property"
-    
+
     def __init__(self):
         self._lista_blanca = self.obtener_lista_blanca()
         self.cofiguracion_detector()
-        
+
     def obtener_lista_blanca(self):
-        return json.loads(json.dumps(os.getenv('ORIGENES_AUTORIZADOS', '[]')))
-        
+        return json.loads(json.dumps(os.getenv("ORIGENES_AUTORIZADOS", "[]")))
+
     def cofiguracion_detector(self):
         self.subscriptor = Subscriptor(token=self.generar_jwt())
-        self.inventory_routing_key = 'inventory-audit'
-        self.nombre_cola_inventory = 'inventory-audit'
-        
+        self.inventory_routing_key = "inventory-audit"
+        self.nombre_cola_inventory = "inventory-audit"
+
     def evento_mensaje_nuevo(self, ch, method, properties, body):
         body_str = body.decode("utf-8")
         self.validar_mensaje(body_str)
         self.validar_intrusion(body_str)
-                
+
     async def start(self):
         await self.subscriptor.suscribirse(
-            nombre_cola=self.nombre_cola_inventory, 
-            routing_key=self.inventory_routing_key, 
-            callback=self.evento_mensaje_nuevo
+            nombre_cola=self.nombre_cola_inventory,
+            routing_key=self.inventory_routing_key,
+            callback=self.evento_mensaje_nuevo,
         )
         while True:
-            pass    
-        
+            pass
+
     def validar_mensaje(self, mensaje):
         deserialize = json.loads(mensaje)
-        resumen = hash(json.dumps(deserialize.get('message')))
-        if(resumen != deserialize.get('check_sum')):
+        resumen = hash(json.dumps(deserialize.get("message")))
+        if resumen != deserialize.get("check_sum"):
             logger.warning(f"Mensaje alterado|{mensaje}")
-            
+
     def validar_intrusion(self, mensaje):
         deserialize = json.loads(mensaje)
-        host_ip = deserialize.get('message').get('host_ip')
-        if(not self._lista_blanca.__contains__(host_ip)):
-            logger.warning(f"Mensaje con origen no válido desde la IP {host_ip}|{mensaje}")
-            
+        host_ip = deserialize.get("message").get("host_ip")
+        if not self._lista_blanca.__contains__(host_ip):
+            logger.warning(
+                f"Mensaje con origen no válido desde la IP {host_ip}|{mensaje}"
+            )
+
     def generar_jwt(self):
-        payload = {
-            "sub": "detector",  # Username
-            "aud": "inventario-test",   # Must match RabbitMQ's configured audience
-            "iat": datetime.now(),  # Issued at
-            "exp": datetime.now() + timedelta(minutes=7200)  # Expiration time
-        }
-        return jwt.encode(payload, "inventario-test-key", algorithm="HS256")
+        response = requests.post(
+            "https://autorizador:4443/token/detector", verify=False
+        )
+        token = response.json()["token"]
+        return token
